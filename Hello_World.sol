acfb328cf6cde22969e1e1e0bcc569a9272954f4
tubaamir <135152116+tubaamir@users.noreply.github.com>
2023-10-07 20:37:52 +0530

    Create Hello_World.sol

---
+++ Hello_World.sol
@@ -0,0 +1,9 @@
+//SPDX-Liense-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    function print() public pure returns (string memory) {
+        return 'Hello world!' ;
+    }
+}
